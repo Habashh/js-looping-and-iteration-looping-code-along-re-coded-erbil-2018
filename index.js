2317acab14f13f1cfa308ba5823089fdcc016a73
zaid85j <zaid85j@gmail.com>
2018-11-15 18:15:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+var printbadges=['Lisa', 'Kaitlin', 'Jan'];
+function printBadges(){
+  for(var i=0; i <= printbadges.length; i++){
+  console.log(printbadges[i]);
+  }
+}printBadges();
+
+
+var printbadges=['Joe', 'Gabe'];
+ 
+  for(var i=0; i<= printbadges.length; i++){
+  console.log(`Welcome ${printbadges[i]}! You are employee #${i}.`);
+  }
